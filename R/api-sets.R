ba5f07c65585dc8db92963b9a20e9419a8153408
C. Lente <7017340+clente@users.noreply.github.com>
2021-12-14 00:19:14 -0300

    Add extra details to scry-sets

--- R/api-sets.R
+++ R/api-sets.R
@@ -1,7 +1,15 @@
 #' Retrieve information about a set
 #'
+#' @details A Set object represents a group of related Magic cards. All Card
+#' objects on Scryfall belong to exactly one set.
+#'
+#' Due to Magic's long and complicated history, Scryfall includes many
+#' un-official sets as a way to group promotional or outlier cards together.
+#' Such sets will likely have a code that begins with `p` or `t`, such as `pcel`
+#' or `tori`. Official sets always have a three-letter set code, such as `zen`.
+#'
 #' @section Set types:
-#' An exhaustive list of `set_types` is listed below:
+#' An exhaustive list of `set_types` is reproduced below:
 #' * `core`: A yearly Magic core set (Tenth Edition, etc)
 #' * `expansion`: A rotational expansion set in a block (Zendikar, etc)
 #' * `masters`: A reprint set that contains no new cards (Modern Masters, etc)
@@ -30,39 +38,39 @@
 #' code can be either the `code` or the `mtgo_code` for the set.
 #' @param id The `tcgplayer_id` or `groupId`.
 #' @return A data frame with 1 row and the following columns:
-#' * `object` \[String\]: Set.
-#' * `id` \[UUID\]: A unique ID for this set on Scryfall that will not change.
-#' * `code` \[String\]: The unique three to five-letter code for this set.
-#' * `mtgo_code` \[String\]: The unique code for this set on MTGO, which may
+#' * `object` \[chr\]: Set.
+#' * `id` \[chr\]: A unique ID for this set on Scryfall that will not change.
+#' * `code` \[chr\]: The unique three to five-letter code for this set.
+#' * `mtgo_code` \[chr\]: The unique code for this set on MTGO, which may differ
+#'   from the regular code.
+#' * `arena_code` \[chr\]: The unique code for this set on MTG Arena, which may
 #'   differ from the regular code.
-#' * `arena_code` \[String\]: The unique code for this set on MTG Arena, which
-#'   may differ from the regular code.
-#' * `tcgplayer_id` \[Integer\]: This set’s ID on TCGplayer’s API, also known as
-#'   the `groupId`.
-#' * `name` \[String\]: The English name of the set.
-#' * `set_type` \[String\]: A computer-readable classification for this set. See
+#' * `tcgplayer_id` \[int\]: This set’s ID on TCGplayer’s API, also known as the
+#'   `groupId`.
+#' * `name` \[chr\]: The English name of the set.
+#' * `set_type` \[chr\]: A computer-readable classification for this set. See
 #'   below.
-#' * `uri` \[URI\]: A link to this set object on Scryfall’s API.
-#' * `scryfall_uri` \[URI\]: A link to this set’s permapage on Scryfall’s
-#' * `search_uri` \[URI\]: A Scryfall API URI that you can request to begin
+#' * `uri` \[chr\]: A link to this set object on Scryfall’s API.
+#' * `scryfall_uri` \[chr\]: A link to this set’s permapage on Scryfall’s
+#' * `search_uri` \[chr\]: A Scryfall API URI that you can request to begin
 #'   paginating over the cards in this set.
-#' * `released_at` \[Date\]: The date the set was released or the first card was
+#' * `released_at` \[date\]: The date the set was released or the first card was
 #'   printed in the set (in GMT-8 Pacific time).
-#' * `set_type` \[String\]: Scryfall provides an overall categorization for each
+#' * `set_type` \[chr\]: Scryfall provides an overall categorization for each
 #'   Set in the `set_type` property. An exhaustive list of `set_types` is listed
 #'   in a separate section below.
-#' * `card_count` \[Integer\]: The number of cards in this set.
-#' * `parent_set_code` \[String\]: The set code for the parent set, if any.
-#'   `promo` and `token` sets often have a parent set.
-#' * `printed_size` \[Integer\]: The denominator for the set’s printed collector
+#' * `card_count` \[int\]: The number of cards in this set.
+#' * `parent_set_code` \[chr\]: The set code for the parent set, if any. `promo`
+#'   and `token` sets often have a parent set.
+#' * `printed_size` \[int\]: The denominator for the set’s printed collector
 #'   numbers.
-#' * `digital` \[Boolean\]: True if this set was only released in a video game.
-#' * `nonfoil_only` \[Boolean\]: True if this set contains only nonfoil cards.
+#' * `digital` \[lgl\]: True if this set was only released in a video game.
+#' * `nonfoil_only` \[lgl\]: True if this set contains only nonfoil cards.
 #'   website.
-#' * `foil_only` \[Boolean\]: True if this set contains only foil cards.
-#' * `block_code` \[String\]: Nullable  The block code for this set, if any.
-#' * `block` \[String\]: The block or group name code for this set, if any.
-#' * `icon_svg_uri` \[URI\]: A URI to an SVG file for this set’s icon on
+#' * `foil_only` \[lgl\]: True if this set contains only foil cards.
+#' * `block_code` \[chr\]: Nullable  The block code for this set, if any.
+#' * `block` \[chr\]: The block or group name code for this set, if any.
+#' * `icon_svg_uri` \[chr\]: A URI to an SVG file for this set’s icon on
 #'   Scryfall’s CDN. Hotlinking this image isn’t recommended, because it may
 #'   change slightly over time. You should download it and use it locally for
 #'   your particular user interface needs.
