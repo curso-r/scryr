3e456e7f2fe70f3382954acc143ac7004fb3ecb5
C. Lente <7017340+clente@users.noreply.github.com>
2022-01-04 17:47:36 -0300

    Refactor scry-symbology docs

--- R/api-symbology.R
+++ R/api-symbology.R
@@ -1,42 +1,36 @@
 #' Retrieve information about symbols
 #'
 #' @description
-#' A Card Symbol object represents an illustrated symbol that may appear in
-#' card's mana cost or Oracle text. Symbols are based on the notation used in
-#' the Comprehensive Rules.
+#' An illustrated symbol may appear in card's mana cost or Oracle text. Symbols
+#' are based on the notation used in the Comprehensive Rules.
 #'
 #' For more information about how the Scryfall API represents mana and costs,
 #' see `vignette("colors")`.
 #'
 #' @return A data frame with 1 row and the following columns:
-#' * `object` \[chr\]: A content type for this object, always `card_symbol`.
-#' * `symbol` \[chr\]: The plaintext symbol. Often surrounded with curly braces
-#'   `{}`. Note that not all symbols are ASCII text (for example, `{∞}`).
-#' * `svg_uri` \[chr\]: A URI to an SVG image of this symbol on Scryfall’s CDNs.
+#' * `symbol` \[chr\]: The plaintext symbol. Often surrounded with curly braces.
+#'   Note that not all symbols are ASCII text (for example, `{∞}`).
+#' * `colors` \[list\]: A vector of colors that this symbol represents.
+#' * `cmc` \[dbl\]: A decimal number representing this symbol’s converted mana
+#'   cost. Note that mana symbols from funny sets can have fractional converted
+#'   mana costs.
 #' * `loose_variant` \[chr\]: An alternate version of this symbol, if it is
 #'   possible to write it without curly braces.
 #' * `english` \[chr\]: An English snippet that describes this symbol.
-#'   Appropriate for use in `alt` text or other accessible communication
-#'   formats.
-#' * `transposable` \[lgl\]: True if it is possible to write this symbol
-#'   “backwards”. For example, the official symbol `{U/P}` is sometimes written
-#'   as `{P/U}` or `{P\U}` in informal settings. Note that the Scryfall API
-#'   **never** writes symbols backwards in other responses. This field is
-#'   provided for informational purposes.
-#' * `represents_mana` \[lgl\]: True if this is a mana symbol.
-#' * `cmc` \[num\]: A decimal number representing this symbol’s converted mana
-#'   cost. Note that mana symbols from funny sets can have fractional converted
-#'   mana costs.
-#' * `appears_in_mana_costs` \[lgl\]: True if this symbol appears in a mana cost
-#'   on any Magic card. For example `{20}` has this field set to `false` because
-#'   `{20}` only appears in Oracle text, not mana costs.
-#' * `cmc` \[dbl\]: The mana value of this symbol.
-#' * `funny` \[lgl\]: True if this symbol is only used on funny cards or
-#'   Un-cards.
-#' * `colors` \[list\]: An array of colors that this symbol represents.
-#' * `gatherer_alternates` \[list\]: An array of plaintext versions of this
+#'   Appropriate for use in alt text or other accessible communication formats.
+#' * `gatherer_alternates` \[list\]: A vector of plaintext versions of this
 #'   symbol that Gatherer uses on old cards to describe original printed text.
-#'   For example: `{W}` has `["oW", "ooW"]` as alternates.
+#'   For example: `{W}` has `<"oW", "ooW">` as alternates.
+#' * `transposable` \[lgl\]: Whether it is possible to write this symbol
+#'   "backwards". For example, the official symbol `{U/P}` is sometimes written
+#'   as `{P/U}` or `{P\U}` in informal settings.
+#' * `represents_mana` \[lgl\]: Whether this is a mana symbol.
+#' * `appears_in_mana_costs` \[lgl\]: Whether this symbol appears in a mana cost
+#'   on any _Magic_ card. For example `{20}` has this field set to `FALSE`
+#'   because `{20}` only appears in Oracle text, not mana costs.
+#' * `funny` \[lgl\]: Whether this symbol is only used on funny cards or
+#'   Un-cards.
+#' * `svg_uri` \[chr\]: A URI to an SVG image of this symbol on Scryfall’s CDNs.
 #'
 #' @name scry-symbology
 NULL
@@ -54,28 +48,29 @@ scry_symbology <- function() {
 #' Parse mana costs
 #'
 #' @description
-#' Parses the given mana `cost` parameter and returns Scryfall's interpretation.
+#' Parses the given mana `cost` argument and returns Scryfall's interpretation.
 #'
 #' @details
-#' The server understands most community shorthand for mana costs (such as 2WW
-#' for {2}{W}{W}). Symbols can also be out of order, lowercase, or have multiple
-#' colorless costs (such as 2{g}2 for {4}{G}).
+#' The server understands most community shorthand for mana costs (such as `2WW`
+#' for `{2}{W}{W}`). Symbols can also be out of order, lowercase, or have
+#' multiple colorless costs (such as `2{g}2` for `{4}{G}`).
 #'
 #' If part of the string could not be understood, the server will return an
 #' error describing the problem.
 #'
-#' @param cost The mana string to parse. For more information, see section
-#' below.
+#' @param cost The mana string to parse. For more information, see details.
+#'
 #' @return A data frame with 1 row and the following columns:
-#' * `object` \[chr\]: A content type for this object, always `mana_cost`.
 #' * `cost` \[chr\]: The normalized cost, with correctly-ordered and wrapped
 #'   mana symbols.
 #' * `colors` \[list\]: The colors of the given cost.
 #' * `cmc` \[dbl\]: The converted mana cost. If you submit Un-set mana symbols,
 #'   this decimal could include fractional parts.
-#' * `colorless` \[lgl\]: True if the cost is colorless.
-#' * `monocolored` \[lgl\]: True if the cost is monocolored.
-#' * `multicolored` \[lgl\]: True if the cost is multicolored.
+#' * `colorless` \[lgl\]: Whether the cost is colorless.
+#' * `monocolored` \[lgl\]: Whether if the cost is monocolored.
+#' * `multicolored` \[lgl\]: Whether if the cost is multicolored.
+#'
+#' @references <https://scryfall.com/docs/api/card-symbols/parse-mana>
 #'
 #' @export
 parse_cost <- function(cost) {
